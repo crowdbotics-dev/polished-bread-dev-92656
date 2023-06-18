7878be0931ebd7d3e2235b1e6e5cfab85deb89fa
Crowdbotics <engineering@crowdbotics.com>
2023-06-18 23:18:51 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "polished-bread-dev-92656",
+  url: "https://polished-bread-dev-92656.botics.co",
+  api: "https://polished-bread-dev-92656.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
